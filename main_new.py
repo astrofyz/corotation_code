local maxima fix scale and rotation

main_new.py
@@ -1,6 +1,9 @@
+import mod_read
+import mod_analysis
 from mod_read import *
 from mod_analysis import *
 from contextlib import contextmanager
+import importlib
 
 #%%
 table_path = '/media/mouse13/My Passport/corotation/buta_gal/all_table_buta_rad_astrofyz.csv'
@@ -10,7 +13,7 @@ out_path = '/media/mouse13/My Passport/corotation_code/data/newnew/'
 names = np.loadtxt('gal_names.txt', dtype='str')
 # print(names)
 
-images = make_images(names=names[2:10], bands='all', types='all', path=im_path)
+images = make_images(names=names[0:2], bands='all', types='all', path=im_path)
 
 @contextmanager
 def figure(**kw):
@@ -27,43 +30,50 @@ def figure(**kw):
     plt.show()
 
 #%%
+images = [images]
+# print(len(images))
+
+#%%
+importlib.reload(mod_analysis)
 for image in images:
-    try:
+    # try:
         for band in ['g', 'i', 'r', 'u', 'z']:
             find_parabola(image[band])
-        # plot surface brigtness profiles with fitted parabola
-        with figure(xlabel='r (arcsec)', ylabel='$\mu[g, i, r, u, z] \quad (mag\:arcsec^{-2})$', savename=out_path+str(image['objid14'])+'.png') as fig:
-            plt.title('{}\n ra={}; dec={}'.format(image['name'], np.round(image['ra'],3), np.round(image['dec'], 3)))
-            plt.gca().invert_yaxis()
-            for band, color in zip(['g', 'i', 'r', 'u', 'z'], ['blue', 'gold', 'r', 'm', 'g']):
-                plt.plot(image[band]['sb.rad.pix']*0.396, image[band]['sb'], color=color,  label='{} : {}'''.format(band, np.round(image[band]['sb.rad.min'], 3)))
-                plt.fill_between(image[band]['sb.rad.pix']*0.396, image[band]['sb']-image[band]['sb.err'], image[band]['sb']+image[band]['sb.err'], color=color,  alpha=0.2)
-                plt.plot(image[band]['sb.rad.fit']*0.396, image[band]['sb.fit'], color='k')
-                plt.axvline(image[band]['sb.rad.min']*0.396, color=color)
-            plt.legend()
-        # find position angle of bar
-        image['r'].plot_slits(n_slit=40, savename=out_path+str(image['objid14'])+'_slits.png')
 
-        # r_min_slit_0 = find_fancy_parabola(image=False, rad_pix=np.split(np.array(images[0]['r']['slits.rad.pix'][:-1]), 2)[1],
-        #                                    sb_err=np.split(np.array(images[0]['r']['slit.min.err'][:-1]), 2)[1],
-        #                                    r_max=images[0]['r']['r.max.pix'],
-        #                                    sb=np.split(np.array(images[0]['r']['slit.min'][:-1]), 2)[1])
-        #
-        # print(r_min_slit_0)
-        #
+        calc_slit(image['r'], 40, convolve=True)
+        # plot surface brigtness profiles with fitted parabola
+        # with figure(xlabel='r (arcsec)', ylabel='$\mu[g, i, r, u, z] \quad (mag\:arcsec^{-2})$', savename=out_path+str(image['objid14'])+'.png') as fig:
+        #     plt.title('{}\n ra={}; dec={}'.format(image['name'], np.round(image['ra'],3), np.round(image['dec'], 3)))
+        #     plt.gca().invert_yaxis()
+        #     for band, color in zip(['g', 'i', 'r', 'u', 'z'], ['blue', 'gold', 'r', 'm', 'g']):
+        #         plt.plot(image[band]['sb.rad.pix']*0.396, image[band]['sb'], color=color,  label='{} : {}'''.format(band, np.round(image[band]['sb.rad.min'], 3)))
+        #         plt.fill_between(image[band]['sb.rad.pix']*0.396, image[band]['sb']-image[band]['sb.err'], image[band]['sb']+image[band]['sb.err'], color=color,  alpha=0.2)
+        #         plt.plot(image[band]['sb.rad.fit']*0.396, image[band]['sb.fit'], color='k')
+        #         plt.axvline(image[band]['sb.rad.min']*0.396, color=color)
+        #     plt.legend()
+        # find position angle of bar
+        # image['r'].plot_slits(n_slit=40, savename=out_path+str(image['objid14'])+'_slits.png')
+
+        r_min_slit_0 = find_fancy_parabola(image=False, rad_pix=np.split(np.array(image['r']['slits.rad.pix'][:-1]), 2)[1],
+                                           sb_err=np.split(np.array(image['r']['slit.min.err'][:-1]), 2)[1],
+                                           r_max=image['r']['r.max.pix'],
+                                           sb=np.split(np.array(image['r']['slit.min'][:-1]), 2)[1])
+
+        print(r_min_slit_0[-1])
+
         # r_min_slit_1 = find_fancy_parabola(image=False, rad_pix=np.split(np.array(image['r']['slits.rad.pix'][:-1]), 2)[1],
         #                              sb_err=np.split(np.array(image['r']['slit.max.err'][:-1]), 2)[1],
         #                              r_max=image['r']['r.max.pix'],
         #                              sb=np.split(np.array(image['r']['slit.max'][:-1]), 2)[1], plot=True)[-1]
         #
-        # print(r_min_slit_1)
+        # print(r_min_slit_1[-1])
 
         # plot "bar"
-        # with figure(show=True) as fig:
+        with figure(show=True) as fig:
         with figure(savename=out_path+'bar_'+str(image['objid14'])+'.png') as fig:
             plt.title('{}\n ra={}; dec={}'.format(image['name'], np.round(image['ra'], 3), np.round(image['dec'], 3)))
             plt.imshow(image['r']['real.mag'], origin='lower', cmap='Greys',
-                       norm=ImageNormalize(stretch=LinearStretch()))
+                       norm=ImageNormalize(stretch=LinearStretch(slope=1.7)))
             idx = np.argmax([sum(abs(row)) for row in image['r']['residuals']])  # перенести это в функцию
             idx_bar = np.argmax(abs(image['r']['residuals'][idx]))
             # print(image['r']['slits.rad.pix'][idx_bar])
@@ -72,14 +82,17 @@ for image in images:
             aper.plot(lw=0.2, color='blue', label='max_resid')
             aper = CircularAperture([xc, yc], abs(image['r']['sb.rad.min']))
             aper.plot(lw=0.2, color='red', label='corot_r')
+            aper = CircularAperture([xc, yc], r_min_slit_0[-1])
+            aper.plot(lw=0.2, color='red', label='perpendicular')
+            plt.legend()
 
             # aper = CircularAperture([xc, yc], r_min_slit_0)
             # aper.plot(lw=0.2, color='y', label='slit_0')
             # aper = CircularAperture([xc, yc], r_min_slit_1)
             # aper.plot(lw=0.2, color='g', label='slit_1')
-    except:
-        print(image['objid14'], 'none')
-        pass
+    # except:
+    #     print(image['objid14'], 'none')
+    #     pass
 
 
 
@@ -119,3 +132,126 @@ for image in images:
 # plt.show()
 
 # ну это прикольно. а что с этим дальше делать?
+
+
+#%%
+from skimage.filters import unsharp_mask
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.measure import label
+from skimage import data
+from skimage import color
+from skimage.morphology import extrema
+from skimage import exposure
+
+res = unsharp_mask(images[0]['r']['real.mag'], radius=0.01, amount=3)
+img = images[0]['r']['real.mag']
+h = 5.
+h_maxima = extrema.h_maxima(img, h)
+label_h_maxima = label(h_maxima)
+overlay_h = color.label2rgb(label_h_maxima, img, alpha=0.7, bg_label=0,
+                            bg_color=None, colors=[(1, 0, 0)])
+#%%
+print(np.argmax(h_maxima))
+#%%
+fig, axes = plt.subplots(ncols=2, sharex=True, sharey=True)
+axes[0].imshow(images[0]['r']['real.mag'], origin='lower', cmap='Greys',
+                       norm=ImageNormalize(stretch=LinearStretch(slope=1.7)))
+# axes[1].imshow(res, origin='lower', cmap='Greys')
+# print(overlay_h)
+axes[1].imshow(overlay_h[:,:,0], origin='lower', cmap='Greys', norm=ImageNormalize(vmin=10, vmax=40))
+# axes[1].imshow(overlay_h[:,:,1], origin='lower')
+# axes[1].imshow(overlay_h[:,:,2], origin='lower')
+plt.show()
+plt.close()
+
+
+#%%
+print(overlay_h[:, :, 0].shape)
+# print(np.max(overlay_h), np.min(overlay_h))
+
+# print(img.shape)
+# print(np.max(img), np.min(img))
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from scipy import ndimage as ndi
+from skimage.feature import shape_index
+from skimage.draw import circle
+
+image = images[0]['r']['real.mag']
+s = shape_index(image)
+
+# In this example we want to detect 'spherical caps',
+# so we threshold the shape index map to
+# find points which are 'spherical caps' (~1)
+
+target = 20
+delta = 2
+
+point_y, point_x = np.where(np.abs(s - target) < delta)
+point_z = image[point_y, point_x]
+
+# The shape index map relentlessly produces the shape, even that of noise.
+# In order to reduce the impact of noise, we apply a Gaussian filter to it,
+# and show the results once in
+
+s_smooth = ndi.gaussian_filter(s, sigma=10)
+
+point_y_s, point_x_s = np.where(np.abs(s_smooth - target) < delta)
+point_z_s = image[point_y_s, point_x_s]
+
+fig = plt.figure(figsize=(12, 4))
+ax1 = fig.add_subplot(1, 3, 1)
+
+ax1.imshow(image, cmap=plt.cm.gray)
+ax1.axis('off')
+ax1.set_title('Input image')
+
+scatter_settings = dict(alpha=0.75, s=10, linewidths=0)
+
+ax1.scatter(point_x, point_y, color='blue', **scatter_settings)
+ax1.scatter(point_x_s, point_y_s, color='green', **scatter_settings)
+
+ax2 = fig.add_subplot(1, 3, 2, projection='3d', sharex=ax1, sharey=ax1)
+
+x, y = np.meshgrid(
+    np.arange(0, image.shape[0], 1),
+    np.arange(0, image.shape[1], 1)
+)
+
+ax2.plot_surface(x, y, image, linewidth=0, alpha=0.5)
+
+ax2.scatter(
+    point_x,
+    point_y,
+    point_z,
+    color='blue',
+    label='$|s - 1|<0.05$',
+    **scatter_settings
+)
+
+ax2.scatter(
+    point_x_s,
+    point_y_s,
+    point_z_s,
+    color='green',
+    label='$|s\' - 1|<0.05$',
+    **scatter_settings
+)
+
+ax2.legend(loc='lower left')
+
+ax2.axis('off')
+ax2.set_title('3D visualization')
+
+ax3 = fig.add_subplot(1, 3, 3, sharex=ax1, sharey=ax1)
+
+ax3.imshow(s, cmap=plt.cm.gray)
+ax3.axis('off')
+ax3.set_title('Shape index, $\sigma=1$')
+
+fig.tight_layout()
+
+plt.show()

mod_analysis.py
@@ -256,24 +256,6 @@ def find_fancy_parabola(image, **kw):
     max_peak = signal.argrelextrema(curvature[idxs_valid], np.greater)[0]
     possible_bounds = np.sort(np.concatenate([zero_abs, max_peak]))
 
-    # with figure() as fig:
-    #     plt.plot(rad_pix, sb_conv)
-    #     plt.plot(rad_pix, sb)
-    #     plt.axvline(rad_pix[idxs_valid][min_peak])
-    #     plt.vlines(rad_pix[idxs_valid][zero_abs], color='k', ymin=min(sb), ymax=max(sb))
-    #     plt.vlines(rad_pix[idxs_valid][max_peak], color='k', ymin=min(sb), ymax=max(sb))
-    #     plt.show()
-    #
-    # with figure() as fig:
-    #     plt.plot(rad_pix, curvature)
-    #     plt.axhline(0.)
-    #     plt.plot(np.linspace(rad_pix[0], rad_pix[-1], int(len(rad_pix)*2)),
-    #              interp1d(rad_pix, curvature)(np.linspace(rad_pix[0], rad_pix[-1], int(len(rad_pix)*2))))
-    #     plt.axvline(rad_pix[idxs_valid][min_peak])
-    #     plt.vlines(rad_pix[idxs_valid][zero_abs], color='k', ymin=min(curvature), ymax=max(curvature))
-    #     plt.vlines(rad_pix[idxs_valid][max_peak], color='k', ymin=min(curvature), ymax=max(curvature))
-    #     plt.show()
-
     try:
         low = idxs_valid[0][possible_bounds[np.searchsorted(possible_bounds, min_peak)-1]]
         top = idxs_valid[0][possible_bounds[np.searchsorted(possible_bounds, min_peak)]]
@@ -298,6 +280,12 @@ def find_fancy_parabola(image, **kw):
     fit_sb = sb[low:top+1]
     p = np.poly1d(np.polyfit(fit_r, fit_sb, deg=2))
 
+    try:
+        r_min = opt.minimize_scalar(-p, method='Bounded', bounds=[fit_r[0], fit_r[-1]]).x
+    except:
+        r_min = 0.
+        print("couldn't find parabolda minimum")
+
     if 'plot' in kw:
         f, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [4, 2]}, sharex=True, figsize=(8, 10))
         ax1.plot(rad_pix, sb, color='darkred', lw=1, label='profile')
@@ -306,6 +294,7 @@ def find_fancy_parabola(image, **kw):
         ax1.axvline(rad_pix[low], color='y')
         ax1.axvline(rad_pix[top], color='g')
         ax1.axvline(rad_pix[min_peak], color='red')
+        ax1.axvline(r_min, color='orchid', lw=3)
         ax1.set_xlabel('r (arcsec)')
         ax1.set_ylabel('$\mu \quad (mag\:arcsec^{-2})$')
         ax1.legend()
@@ -317,12 +306,6 @@ def find_fancy_parabola(image, **kw):
         plt.grid()
         plt.show()
         plt.close()
-    # нужно что-то записать в класс. посмотреть, что нужно дальше и записать его
-    try:
-        r_min = opt.minimize_scalar(-p, method='Bounded', bounds=[fit_r[0], fit_r[-1]]).x
-    except:
-        r_min = 0.
-        print("couldn't find parabolda minimum")
 
     if isinstance(image, mod_read.ImageClass):
         image.prop(['sb.rad.fit', 'sb.fit', 'sb.rad.min'],
@@ -484,10 +467,10 @@ def calc_slit(image, n_slit=1, angle=0., step=1.2, width=3.5, **kw):
     image.prop('slits.angle', data=pa_space)  # проверь, что размерность совпадает, а не в два раза меньше!
 
     idx = np.argmax([sum(abs(row)) for row in image['residuals']])
-    image.prop('slit.max', data=slits[idx][np.argmax([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
-    image.prop('slit.min', data=slits[idx][np.argmin([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
-    image.prop('slit.max.err', data=errors[idx][np.argmax([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
-    image.prop('slit.min.err', data=errors[idx][np.argmin([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
+    image.prop('slit.min', data=slits[idx][np.argmax([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
+    image.prop('slit.max', data=slits[idx][np.argmin([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
+    image.prop('slit.min.err', data=errors[idx][np.argmax([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
+    image.prop('slit.max.err', data=errors[idx][np.argmin([sum(abs(slits[idx][0])), sum(abs(slits[idx][1]))])])
     image.prop('angle.max', data=pa_space[idx])
     return rad, slits
 

mod_read.py
@@ -82,17 +82,21 @@ class ImageClass(dict):
         ax1.set_title('{}\nra={}, dec={}'.format(self['name'], np.round(self['ra'], 3), np.round(self['dec'], 3)))
         ax1.imshow(self['real.mag'], origin='lower', cmap='Greys')
         # print(self['slits'])
-        for slit in self['slits']:
-            ax2.plot(self['slits.rad.pix'], slit[0], color='orange')
-            ax2.plot(self['slits.rad.pix'], slit[1], color='navy')
+        idx = np.argmax([sum(abs(row)) for row in self['residuals']])
+        for i, slit in enumerate(self['slits']):
+            if i == idx:
+                ax2.plot(self['slits.rad.pix'], slit[0], color='coral', lw=1)
+                ax2.plot(self['slits.rad.pix'], slit[1], color='navy', lw=1)
+            else:
+                ax2.plot(self['slits.rad.pix'], slit[0], color='gold', alpha=0.)
+                ax2.plot(self['slits.rad.pix'], slit[1], color='dodgerblue', alpha=0.)
             ax2.grid()
         ax2.invert_yaxis()
-        idx = np.argmax([sum(abs(row)) for row in self['residuals']])
         for i in range(len(self['residuals'])):
             if i == idx:
                 ax3.plot(self['slits.rad.pix'], self['residuals'][i], color='crimson', label='pa = {}'.format(np.round(self['slits.angle'][idx], 3)))  # add angle to label!
             else:
-                ax3.plot(self['slits.rad.pix'], self['residuals'][i], color='dodgerblue')
+                ax3.plot(self['slits.rad.pix'], self['residuals'][i], color='dodgerblue', alpha=0.2)
             ax3.set_xlabel('r, pix')
             ax3.axhline(0.)
             ax3.legend()