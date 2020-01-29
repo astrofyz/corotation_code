d8a98d9e45067e8ccef474123e8d938f3db1bb03
astrofyz <pushfastdieyoung>
2020-01-29 17:17:48 -0600

    corrected convolution and curvature

--- main_new.py
+++ main_new.py
@@ -8,8 +8,9 @@ import os
 import umap
 from sklearn.cluster import KMeans
 from scipy.optimize import curve_fit
-from scipy.interpolate import UnivariateSpline
-
+import scipy.optimize as opt
+from scipy.interpolate import UnivariateSpline, splrep, splev, sproot
+from time import time
 #%%
 table_path = '/media/mouse13/Seagate Expansion Drive/corotation/buta_gal/all_table_buta_rad_astrofyz.csv'
 im_path = '/media/mouse13/Seagate Expansion Drive/corotation/buta_gal/image'
@@ -145,6 +146,11 @@ def rescale(data, a=-1, b=1):
     max_x = max(data)
     return (b-a)*(data-min_x)/(max_x-min_x) + a
 
+#%%
+images = make_images(names=names[:5], bands='all', types='all', path=im_path, SE=True, calibration=True, correction=True)
+
+#%%
+start = time()
 dn = 6
 n = len(names[:])
 print(n)
@@ -152,16 +158,16 @@ chunk = 0
 for chunk in range(0, n, dn):
     dc = min(chunk+dn, n)
     images = make_images(names=names[chunk:dc], bands='all', types='all', path=im_path, SE=True, calibration=True, correction=True)
-    for image in images:
-        # try:
+    for image in images[:]:
+        try:
             fig, ax = plt.subplots(nrows=2, sharex=True)
-            for band, color in zip(['g', 'r', 'z', 'i', 'u'], ['blue', 'r', 'g', 'gold', 'm']):
+            for band, color in zip(['g', 'r', 'z', 'i', 'u'][:2], ['blue', 'r', 'g', 'gold', 'm'][:2]):
                 calc_sb(image[band], error=True)
                 rad = image[band]['sb.rad.pix']
                 radius = np.linspace(min(rad), max(rad), 500)
                 sb = image[band]['sb']
                 sb_err = image[band]['sb.err']
-                conv_kernel = Gaussian1DKernel(stddev=2. * np.sqrt(np.mean(sb_err)))
+                conv_kernel = Gaussian1DKernel(stddev=3. * np.sqrt(np.mean(sb_err)))
                 sb_conv = convolve(sb, conv_kernel, boundary='extend')
                 fit1d = np.polyfit(rad, sb_conv, 1)
                 diff_1d = sb_conv-np.poly1d(fit1d)(rad)
@@ -169,30 +175,95 @@ for chunk in range(0, n, dn):
                 diff_exp = sb_conv-func(rad, *popt)
                 curvature = find_curvature(rad, sb)
                 curvature_conv = find_curvature(rad, sb_conv)
-                rescale_product = lambda x: interp1d(rad, rescale(diff_1d))(x) \
-                                            * interp1d(rad, rescale(diff_exp))(x) \
-                                            * interp1d(rad, rescale(-1. * curvature_conv))(x)  #да, можно было бы 3 аргумента передавать
-                ax[0].plot(rad, sb, color=color, label=band, lw=1.)
-                # ax[1].plot(radius, rescale_product(radius), label=band, color=color, lw=1., alpha=0.8)
-                ax[1].plot(rad, curvature_conv, label=band, color=color, lw=1., alpha=0.8)
+                # rescale_product = lambda x: interp1d(rad, rescale(diff_1d))(x) \
+                #                             * interp1d(rad, rescale(diff_exp))(x) \
+                #                             * interp1d(rad, rescale(-1. * curvature_conv))(x)  #да, можно было бы 3 аргумента передавать
+                ax[0].plot(rad, sb, color=color, lw=1.)
+                ax[0].plot(rad, sb_conv, color=color, lw=.7, alpha=0.5)
+                ax[1].plot(rad, curvature_conv, color=color, lw=1., alpha=0.6)
                 ax[1].axhline(0., color='k', lw=0.5)
-                # roots_curv = UnivariateSpline(rad, curvature_conv).roots()
-                # ax[1].plot(radius, UnivariateSpline(rad, curvature_conv)(radius))
-                # for root in roots_curv:
-                #     ax[0].axvline(root, color='k', lw=0.5)
+                tck = splrep(rad, curvature_conv)
+                ynew = splev(radius, tck)
+                ax[1].plot(radius, ynew, color=color, lw=1.)
+                roots = sproot(tck, mest=6)
+                odd_flag = int(splev(0.5 * (roots[0] + roots[1]), tck) > 0)  # 0 if negative in first interval
+                intervals = []
+                for i in range(len(roots) - 1)[odd_flag:][::2]:
+                    ax[0].axvline(roots[i], color=color, lw=0.3, alpha=0.1)
+                    ax[0].axvline(roots[i+1], color=color, lw=0.3, alpha=0.1)
+                    min1 = opt.minimize_scalar(lambda x: splev(x, tck), method='Bounded',
+                                               bounds=[roots[i], roots[i + 1]]).x
+                    f_min = splev(min1, tck)
+                    intervals.append([opt.minimize_scalar(lambda y: abs(splev(y, tck) - 0.5 * f_min), method='Bounded',
+                                                          bounds=[min([roots[k], min1]), max(roots[k], min1)]).x for k
+                                      in [i, i + 1]])
+                for interval in intervals:
+                    idxs_rad = np.where((rad < interval[1]) & (rad > interval[0]))
+                    p = np.poly1d(np.polyfit(rad[idxs_rad], sb[idxs_rad], deg=2))
+                    ax[0].plot(rad[idxs_rad], p(rad[idxs_rad]), color='k')
+                    try:
+                        ax[0].axvline(
+                            opt.minimize_scalar(-p, method='Bounded', bounds=[rad[idxs_rad][0], rad[idxs_rad][-1]]).x,
+                            color=color, alpha=0.7, label=np.round(p[2], 3))
+                    except:
+                        print(f'no minimum for this interval {interval}')
             ax[0].invert_yaxis()
             fig.legend()
+            ax[0].set_title('{}'.format(image['name']))
             # fig.show()
             fig.savefig(out_path+f"sb_check/curv_{str(image['objID'])}.png")
             plt.close()
-        # except:
-        #     print(image['objID'], 'none')
-        # pass
+            print(image['name'])
+        except:
+            print(image['objID'], 'none')
+            pass
+print(time()-start)
+#%%
+# print(splev(0.5*(roots[0] + roots[1]), tck))
+odd_flag = int(splev(0.5*(roots[0] + roots[1]), tck) > 0)  # 0 if negative in first interval
+# print(odd_flag)
+min1 = opt.minimize_scalar(lambda x: splev(x, tck), method='Bounded', bounds=[roots[0], roots[1]]).x
+f_min = splev(min1, tck)
+# print(min1, roots[0], roots[1])
+half_roots = [opt.minimize_scalar(lambda y: abs(splev(y, tck)-0.5*f_min), method='Bounded',
+                                  bounds=[min([roots[i], min1]), max(roots[i], min1)]).x for i in range(2)]
+# print(opt.minimize_scalar(lambda y: float(splev(y, tck))+0.5*0.0112, method='Bounded',
+#                                  bounds=[min([roots[0], min1]), max(roots[0], min1)]).x)
+# print(half_roots)
+# print(type(splev(3., tck)))
+
+intervals = []
+for i in range(len(roots)-1)[odd_flag:][::2]:
+    min1 = opt.minimize_scalar(lambda x: splev(x, tck), method='Bounded', bounds=[roots[i], roots[i+1]]).x
+    f_min = splev(min1, tck)
+    intervals.append([opt.minimize_scalar(lambda y: abs(splev(y, tck) - 0.5 * f_min), method='Bounded',
+                                      bounds=[min([roots[k], min1]), max(roots[k], min1)]).x for k in [i, i+1]])
+
+# print(intervals)
+
+with figure(1) as fig:
+    for interval in intervals:
+        # print(interval)
+        idxs_rad = np.where((rad < interval[1])&(rad>interval[0]))
+        p = np.poly1d(np.polyfit(rad[idxs_rad], sb[idxs_rad], deg=2, full=True)[0])
+        print(p)
+        print(p[0], p[2])
+        print(np.polyfit(rad[idxs_rad], sb[idxs_rad], deg=2, full=True))
+        plt.plot(rad, sb, alpha=0.3)
+        plt.plot(rad[idxs_rad], p(rad[idxs_rad]))
+        try:
+            plt.axvline(opt.minimize_scalar(-p, method='Bounded', bounds=[rad[idxs_rad][0], rad[idxs_rad][-1]]).x)
+            # print(opt.minimize_scalar(-p, method='Bounded', bounds=interval).x)
+        except:
+            print(f'no minimum for this interval {interval}')
+
 
 #%%
+# tck = splrep(rad, curvature_conv)
+# ynew = splev(radius, tck)
 # with figure(1) as fig:
-#     plt.scatter(rad, sb, marker='.')
-#     plt.plot(rad, np.poly1d(fit1d)(rad))
+#     plt.scatter(rad, curvature_conv, marker='.')
+#     plt.plot(radius, ynew)
 
 # with figure(2) as fig:
 #     plt.scatter(rad, sb-np.poly1d(fit1d)(rad), marker='.')
